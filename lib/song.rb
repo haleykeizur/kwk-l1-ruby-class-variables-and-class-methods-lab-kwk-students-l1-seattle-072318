3dd7c2e5ac33a2b5897385b04271e76662a94868
haleykeizur <haleykz@yahoo.com>
2018-07-30 16:12:17 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,6 +4,11 @@ class Song
   attr_accessor :artist
   attr_accessor :genre
   
+  @@count = 0 
+  def self.count
+    @@count
+  end 
+  
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
